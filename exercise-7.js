c8a73ba5f9a0d126c2a6da261956496e1b100adc
FrancescoCammarata <122449737+FrancescoCammarata@users.noreply.github.com>
2023-02-18 19:21:48 +0100

    Exercises 58 - Getters and Setters

---
+++ exercise-7.js
@@ -0,0 +1,43 @@
+class Person {
+  #firstName;
+  #lastName;
+  #age;
+
+  constructor(firstName, lastName, age){
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
+  }
+
+  get firstName(){
+    return this.#firstName;
+  }
+  set firstName(firstName){
+    this.#firstName = firstName;
+    return this;
+  }
+  get lastName(){
+    return this.#lastName;
+  }
+  set lastName(lastName){
+    this.#lastName = lastName;
+    return this;
+  }
+  get age(){
+    return this.#age;
+  }
+  set age(age){
+    this.#age = age;
+    return this;
+  }
+  get fullName(){
+    return `${this.#firstName} ${this.#lastName}`
+  }
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
